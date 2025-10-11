9b7de4575fdcc6065c7997aef9b734c4c16b313a
Steven Chen <stevenshine@hotmail.co.uk>
2025-10-11 13:48:57 +0100

    Move process votes and actions outside of parser

--- textarena/envs/Avalon/env.py
+++ textarena/envs/Avalon/env.py
@@ -298,18 +298,16 @@ class AvalonParser:
         m = AvalonParser.action_pattern.search(text)
         return m.group(1).lower() if m else None
 
-    @staticmethod
-    def process_mission_actions(mission_actions: Dict[int, str]) -> bool:
-        success = all(action == "success" for action in mission_actions.values())
-        return success
-    
-    @staticmethod
-    def process_team_votes(votes: Dict[int, str]) -> bool:
-        approve_count = sum(1 for v in votes.values() if v == "approve")
-        reject_count = len(votes) - approve_count
+def is_mission_success(mission_actions: Dict[int, str]) -> bool:
+    success = all(action == "success" for action in mission_actions.values())
+    return success
 
-        success = approve_count > reject_count
-        return success
+def is_team_proposal_passed(votes: Dict[int, str]) -> bool:
+    approve_count = sum(1 for v in votes.values() if v == "approve")
+    reject_count = len(votes) - approve_count
+
+    success = approve_count > reject_count
+    return success
 
 class VoteHandler:
     @staticmethod
