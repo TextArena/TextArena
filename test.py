2cc05e5f39021836e6561ff55684f48dc2780996
bobbycxy <bobbycxy1994@gmail.com>
2026-08-18 17:39:32 +0800

    updates

--- test.py
+++ test.py
@@ -1,22 +1,31 @@
-""" A minimal script showing how to run textarena locally """
+"""A minimal script showing how to run TextArena locally."""
 
-import textarena as ta 
+import textarena as ta
 
 agents = {
     0: ta.agents.HumanAgent(),
-    1: ta.agents.HumanAgent(),
+    1: ta.agents.HumanAgent()
 }
 
-# initialize the environment
+# Initialize the environment
 env = ta.make(env_id="TicTacToe-v0-train")
-env.reset(num_players=len(agents), lang_mapping={0: "zh", 1: "en"}) # You can also just pass a single lang string if all players will use the same one, e.g. lang="en"
 
-# main game loop
-done = False 
+# Reset the environment with the number of players
+env.reset(num_players=len(agents))
+
+# Alternatively, assign a language to each player
+# env.reset(
+#     num_players=len(agents),
+#     lang_mapping={0: "en", 1: "de"}
+# )
+
+done = False
 while not done:
-  player_id, observation = env.get_observation()
-  action = agents[player_id](observation)
-  done, step_info = env.step(action=action)
+    player_id, observation = env.get_observation()
+    action = agents[player_id](observation)
+    done, step_info = env.step(action)
+
 rewards, game_info = env.close()
+
 print(rewards)
 print(game_info)
